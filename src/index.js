3af7860f73079aaaac0b7a3dd0a9aae6003dbcba
Matsukevich Yuliya <july.matsukevich@mail.ru>
2023-09-22 12:39:53 +0300

    add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+  var fromZeroToNine = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  let numberToStr = number.toString();
+  const length = numberToStr.length;
+  let result ='';
+
+  for (let i = length; i > 0; i--) {
+    if (i > 2) {
+        result += `${fromZeroToNine[numberToStr[length - i]]} hundred `; 
+    } else if (i > 1 && numberToStr[length - i] > 1) {
+       result += `${tens[numberToStr[length - i]]} `; 
+    } else if (i > 1 && numberToStr[length - i] == 1){
+        result += `${teens[numberToStr[length - i + 1]]} `;
+        i--;
+    } else if (i == 1) {
+        if (number == 0 || (numberToStr[length - i] != 0)) {
+            result += `${fromZeroToNine[numberToStr[length - i]]} `; 
+        }
+    }
+  }
+  return result.trim();
 }
